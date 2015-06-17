45c698ca962f7d2007f6c12c378cdedc4aa7e3d6
Philip E Blair <peblairman@gmail.com>
2015-06-17 16:21:57 -0400

    Cleaned up a little and began documenting the image example module language

--- docs/image-helpers.rkt
+++ docs/image-helpers.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang racket/base
 
 #|
 ## IMAGE EXAMPLE HELPERS
@@ -9,28 +9,31 @@
 ## examples
 |#
 
-;; TODO: Clean up requires
-(require ;"scribble-api.rkt" "written/abbrevs.rkt" 
-         (except-in scribble/eval examples)
-          2htdp/image
-          scribble/base
-          scribble/core scribble/html-properties
+(require (except-in scribble/eval examples)
           racket/runtime-path
           racket/base
-          racket/system
+          racket/match
+          racket/contract
           racket/pretty
-          (prefix-in xml: xml)
-          (for-meta 2 racket/base)
-          (for-template scribble/core scribble/html-properties)
-          (for-syntax racket/base syntax/parse racket/list))
+          (only-in racket/port with-output-to-string port->string)
+          (only-in racket/system system)
+          (only-in racket/file make-temporary-file)
+          (only-in racket/string string-join string-split)
+          (only-in racket/path file-name-from-path)
+          (only-in racket/list flatten)
+          (for-syntax racket/base))
 
 (provide USE-PYRET
          racket-comment
          process-examples
-         respace-example
-         prettify/pyret)
+         respace-example)
 
+;; Flag for rendering all images with their
+;; bounding boxes and center points
 (define DEBUG-IMAGES #t)
+
+;; Runtime paths relative to this file
+;; (i.e. relative to docs/)
 
 ;; Path to phase 1 main-wrapper.js
 (define-runtime-path pyret-compiler
@@ -45,14 +48,14 @@
 (runtime-require scribble-api)
 (runtime-require abbrevs)
 
-#;(define-runtime-path matrices-arr
-  (build-path ".." ".." "junk-drawer" "pyret" "trove" "matrices.arr"))
-
+;; Locates path for node.js
 (define node
   (match (system-type)
     ['windows (find-executable-path "node.exe")]
     [else     (find-executable-path "node"    )]))
 
+;; Runs the given Pyret file and returns the
+;; program's output to stdout
 (define/contract (get-pyret-output file)
   (path? . -> . string?)
   (let ((command (string-append "node "
@@ -68,8 +71,16 @@
 ;; If we can't find the compiler, just fall back on Racket output
 (define USE-PYRET (and (file-exists? pyret-compiler) (file-exists? node)))
 
-(define (make-pyret-temp-file)
-  (make-temporary-file "pyretdoctmp~a.arr" #f #f))
+;; Creates a new temporary file (to hold the examples)
+(define (make-pyret-temp-file (fail-thunk #f))
+  ;; Need to catch exn:fail? here for cases when the
+  ;; background expander calls this function (in which
+  ;; case the temporary file creation will fail, and
+  ;; an exn:fail? exception is raised)
+  (with-handlers ([exn:fail? (λ(e) (if fail-thunk
+                                       (fail-thunk)
+                                       (raise e)))])
+    (make-temporary-file "pyretdoctmp~a.arr" #f #f)))
 
 ;; A little bit of reflection is needed,
 ;; so force this import
@@ -79,18 +90,32 @@
           'ALL-GEN-DOCS #t #f
           (module->namespace scribble-api))))
 
+;; Returns a list of identifiers which the given module exports,
+;; as per what has been registered with ALL-GEN-DOCS.
+;;
+;; This is used so that we can import all identifiers from the
+;; pyret-image module without any type of namespace prefix
+;; (This methods always makes sure to import even undocumented
+;;  exports, making it more preferable over doing something like
+;;  generating an import statement from the identifiers which
+;;  have been documented)
 (define (module-exports modname)
-  (let ((mod (findf (λ(m)(equal? (second m) modname)) ALL-GEN-DOCS))
+  (let ((mod (findf (λ(m)(equal? (cadr m) modname)) ALL-GEN-DOCS))
         (get-name 
-         (λ(lst)  (second (findf 
+         (λ(lst)  (cadr (findf 
                            (λ(eltlst) 
                              (and (list? eltlst) 
-                                  (equal? (first eltlst) 'name))) lst)))))
+                                  (equal? (car eltlst) 'name))) lst)))))
     (map get-name (cdddr mod))))
 
+;; The actual import statement for the example file
 (define IMAGE-IMPORT-STATEMENT
-  (string-append "import " (string-join (module-exports "pyret-image") ", ") " from pyret-image"))
+  (string-append "import " (string-join (module-exports "pyret-image") ", ")
+                 " from pyret-image"))
 
+;; Wraps the given (example-identifier . example-source) pair in a pyret
+;; expression which, when the file is run, will print out the identifier
+;; and *evaluated* example-source as a dotted pair of the same style.
 (define (prep-image-example pair)
   (match pair
     [(cons sym exmp)
@@ -103,18 +128,16 @@
                     (fixup-str exmp #f)
                     ").tosource()) + ')')")]))
 
+;; Wraps each example in the given association list (see prep-image-example)
 (define (prep-pyret-file-contents examples)
-  ;; print( '(' + <examples (joined with ' + ')> + ')' )
-  ;(string-append "print('(' + " 
-                 (map prep-image-example examples))
-                 ;" + ')')"))
+  (map prep-image-example examples))
 
+;; When successful, the last portion of the program output should be
+;; "The program didn't define any tests.", as opposed to some error
+;; message.
 (define (check-didnt-fail port)
-  (cons? (regexp-match #px"The program didn't define any tests[.\n]{,5}$" (port->string port))))
-
-(define (fix-spacing str)
-  (regexp-replace* #rx" (?:(?!cases|constructor)([_a-zA-Z][_a-zA-Z0-9]*(?:-+[_a-zA-Z0-9]+)*)) \\("
-                   str " \\1("))
+  (regexp-match? #px"The program didn't define any tests[.\n]{,5}$"
+                 (port->string port)))
 
 #|
 ## How this whole thing works:
@@ -138,81 +161,116 @@
 |#
 (define (process-examples examples return-continuation)
   (define examples-in-file (make-hash))
-    
-  (let* ((file (make-pyret-temp-file))
+  (let* ((file (make-pyret-temp-file (λ()(return-continuation examples))))
          (prepped (prep-pyret-file-contents examples))
-         (fileport (with-handlers ([exn:fail? (λ(e)(return-continuation examples))])
+         (fileport (with-handlers ([exn:fail?
+                                    (λ(e)(return-continuation examples))])
                      (open-output-file file  #:exists 'replace))))
-    
+    ;; The name of the fresh temporary file
     (define filename (file-name-from-path file))
+    ;; Matches Pyret error messages referencing the
+    ;; temporary file
     (define filename-rx (regexp (format "~a: line ([0-9]+)" filename)))
+    ;; Returns the region of the temporary file
+    ;; which spans the (area near the) line range
+    ;; provided, formatted with line numbers.
     (define (get-line-range start end)
       (let* ((best-start (get-best-line start))
              (best-end (get-best-line end))
-             (rel-keys (sort (filter (λ(x)(<= best-start x best-end)) (hash-keys examples-in-file)) <))
-             (rel-lines (flatten (map (λ(key)(string-split (hash-ref examples-in-file key) "\n")) rel-keys)))
-             (line-padding (λ(lineno) (list->string (build-list (- (string-length (number->string best-end))
-                                                                   (string-length (number->string lineno))) (λ(n)#\Space))))))
+             (rel-keys (sort (filter (λ(x)(<= best-start x best-end))
+                                     (hash-keys examples-in-file)) <))
+             (rel-lines (flatten (map (λ(key)(string-split
+                                              (hash-ref examples-in-file key)
+                                              "\n")) rel-keys)))
+             (line-padding (λ(lineno)
+                             (list->string
+                              (build-list
+                               (- (string-length (number->string best-end))
+                                  (string-length (number->string lineno)))
+                               (λ(n)#\Space))))))
         (string-join (for/list ((lineno (in-range best-start best-end))
                                 (linestr rel-lines))
-                       (format "~a~a  ~a" (line-padding lineno) lineno linestr)) "\n")))
-        
-        
+                       (format "~a~a  ~a" (line-padding lineno)
+                               lineno linestr)) "\n")))
+    ;; For tracking the locations of examples in the
+    ;; examples file
     (port-count-lines! fileport)
     (define (register-example example)
       (let-values (((line col pos) (port-next-location fileport)))
         (if (hash-has-key? examples-in-file line)
-            (raise (λ()(error (format "Multiple expressions on one line: ~nline: ~a~nold contents: ~a~nnew contents: ~a" 
-                           line (hash-ref examples-in-file line) example))))
+            (error (format (string-append "Multiple expressions on"
+                                          " one line: ~nline: ~a~nold "
+                                          "contents: ~a~nnew contents: ~a")
+                           line (hash-ref examples-in-file line) example))
             (hash-set! examples-in-file line example))))
     (define (get-best-line lineno)
+      ;; Creates a function which returns the
+      ;; maximum of the two given values, so
+      ;; long as it is smaller than the given
+      ;; x value
       (define (max-<= x)
         (λ(a b) (if (or (> a x) (> b a)) b a)))
       ;; First example line is on line 2
       (foldr (max-<= lineno) 2 (hash-keys examples-in-file)))
+    ;; Returns a formatted error message which displays the
+    ;; example located where Pyret raised an error
     (define (fetch-line lineno)
       (let ((bestline (get-best-line lineno)))
         (format (string-append 
                  "~nRegion of error:~n~a"
-                 "~n~nHint: It looks like the problem might be with this example: ~n~a~n~n")
+                 "~n~nHint: It looks like the problem "
+                 "might be with this example: ~n~a~n~n")
                 (get-line-range (- lineno 10) (+ lineno 10))
                 (hash-ref examples-in-file bestline))))
+    ;; If there is a line number reference in the Pyret stack
+    ;; trace provided, returns a formatted error message with
+    ;; the example which was located at that line number
     (define (fetch-from-error errmsg)
       (let ((rxp-startRow (regexp-match #rx"startRow: ([0-9]+)" errmsg))
             (rxp-pyret-stack (regexp-match filename-rx errmsg)))
-        (cond [(pair? rxp-startRow) (fetch-line (string->number (cadr rxp-startRow)))]
-              [(pair? rxp-pyret-stack) (fetch-line (string->number (cadr rxp-pyret-stack)))]
+        (cond [(pair? rxp-startRow) (fetch-line
+                                     (string->number (cadr rxp-startRow)))]
+              [(pair? rxp-pyret-stack) (fetch-line
+                                        (string->number (cadr rxp-pyret-stack)))]
               [else   ""])))
+    ;; Add the image import statement to the examples file on the first line
     (display IMAGE-IMPORT-STATEMENT fileport)
     (newline fileport)
+    ;; Add each of the prepared example strings to the examples file
+    ;; and register their locations
     (for ((printstmt prepped))
       (register-example printstmt)
       (display printstmt fileport)
-      
-      (newline fileport)
-      )
+      (newline fileport))
     (close-output-port fileport)
+    ;; Status indicators: the fun way
     (eprintf ".")
+    ;; Now that all examples are added, run the file
+    ;; through Pyret and collect the output
     (define pyret-output (get-pyret-output file))
+    ;; If Pyret raises an error, spit out our error message
     (unless (check-didnt-fail (open-input-string pyret-output))
-      (newline)(error 
-                           (format "Pyret raised an error while running your examples: \n ~a~a" 
-                                   pyret-output
-                                   (fetch-from-error pyret-output))))
-    (let loop ((result (open-input-string pyret-output))
-               (sofar '()))
-              (let ((in (read result)))
+      (newline)(error (format "Pyret raised an error while running your examples: \n ~a~a" 
+                              pyret-output
+                              (fetch-from-error pyret-output))))
+    ;; If Pyret does not raise an error, collect the examples and return:
+    (let loop ((result (open-input-string pyret-output)) ; <- stdout as a string
+               (sofar '())) ; <- the collected examples thus far
+              (let ((in (read result))) ; <- we can use the standard reader to get an example
+                                        ;    (hence the decision to serialize the results out
+                                        ;     as a series of dotted pairs)
                 (cond [(eof-object? in) (reverse (cdr sofar))] ;; The first value of sofar will be the 
                                                                ;; #t return value from (system ...)
-                      [(cons? in) (loop result (cons in sofar))]
+                      [(pair? in) (loop result (cons in sofar))]
                       [else (loop result sofar)])))))
 
 (pretty-print-columns 50)
 (define (racket-comment datum)
   (define EXAMPLES-PREFIX "# Racket equivalent: ")
-  (define BUFFER (list->string (cons #\# (build-list 
-                                          (sub1 (string-length EXAMPLES-PREFIX)) 
-                                          (λ(n)#\Space)))))
+  (define BUFFER (list->string
+                  (cons #\# (build-list 
+                             (sub1 (string-length EXAMPLES-PREFIX)) 
+                             (λ(n)#\Space)))))
   (define prt (open-output-string))
   (pretty-write datum prt)
   (define pretty-str (begin0 (get-output-string prt)
@@ -221,22 +279,29 @@
     (cons (string-append EXAMPLES-PREFIX (car split-up)) 
           (map (λ(s)(string-append BUFFER s)) (cdr split-up)))))
 
-(define/contract (prettify/pyret example)
-  (string? . -> . string?)
-  (define prt (open-output-string))
-  (parameterize ([pretty-print-columns 80])
-    (pretty-display (prep-example-string example) prt))
-  (begin0 (fixup-str (get-output-string prt))
-          (close-output-port prt)))
-
+;; Reformats the given string to be more suitable for
+;; Pyret evaluation (non-string examples in the @image-examples
+;; macro tend to raise errors due to the reader placing
+;; spaces between function names and arguments)
 (define (fixup-str s (drop-parens? #t))
+  ;; Matches the first and last parentheses of the given string
+  ;; (which are located at the start and end)
   (define FIRSTLASTPAREN #rx"(?:^\\()|(?:\\)$)")
+  ;; Locates any instances of parentheses which have a
+  ;; tick mark or spaces preceding them
   (define PARENRX #px"\\s*'?\\(")
+  ;; Locates any commas located on their own line
   (define SINGLECOMMARX #px"\\s*\n\\s*,[ ]*\n")
+  ;; Locates any commas surrounded by spaces
   (define SPACECOMMA #px"\\s*,(\\s*)")
+  ;; Takes a series of regexp replacements via a varargs
+  ;; association list and performs (sequentially in the
+  ;; order given) each substitution on the given string.
   (define (do-replacements s . rpl)
-    (if (empty? rpl) s
+    (if (null? rpl) s
         (apply do-replacements (regexp-replace* (caar rpl) s (cdar rpl)) (cdr rpl))))
+  ;; Does the replacements, dropping the first and
+  ;; last parentheses as needed
   (if drop-parens?
       (do-replacements s
                        (cons PARENRX "(")
@@ -247,19 +312,6 @@
                        (cons PARENRX "(")
                        (cons SINGLECOMMARX ",\n")
                        (cons SPACECOMMA ",\\1"))))
-
-(define (prep-example-string s)
-  (define raw-read (read (open-input-string (string-append "(" s ")"))))
-  (define (smartcons a d)
-    (match a
-      [(list "unquote" sym) (cons "," (cons sym d))]
-      [else (cons a d)]))
-  (define (deep-map/splice f lst)
-    (cond [(empty? lst) lst]
-          [(cons? (car lst)) (smartcons (deep-map/splice f (car lst))
-                                   (deep-map/splice f (cdr lst)))]
-          [else (smartcons (f (car lst)) (deep-map/splice f (cdr lst)))]))
-  (deep-map/splice (λ(s)(format "~a" s)) raw-read))
 
 (define (trim-max n str)
   (define pattern (pregexp (format "^\\s{,~a}(.*)$" n)))
--- docs/image-modlang.rkt
+++ docs/image-modlang.rkt
@@ -1,9 +1,12 @@
 #lang scribble/base
+
+;; Defines a module language which extends scribble/base
+;; with Pyret image example preprocessing
+
 @(require "image-syntax.rkt"
           (except-in racket/base #%module-begin)
           scribble/base
           scribble/core
-          
           (for-meta 2 racket/base)
           (for-syntax 
            racket/path
@@ -17,6 +20,8 @@
           (rename-out (modbegin #%module-begin))
           image-examples)
 
+;; Runs the source through the image example preprocessor
+;; before delegating to the standard #%module-begin form
 @(define-syntax (modbegin stx)
    (syntax-case stx ()
      [(_ . body)
@@ -29,5 +34,6 @@
           (eprintf "done.~n")
           (quasisyntax/loc stx
             (#%module-begin 
-             doc values ()
+             doc values () ;; <- these three expressions needed
+                           ;;    for the scribble/base/lang #%module-begin form
              #,@processed))))]))
